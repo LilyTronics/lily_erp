011096365fc0ee38a2655e87ec1e72ff049cb39a
Danny van der Pol <danny.vanderpol@teledyne.com>
2024-10-09 12:54:39 +0200

    add minimum application

---
+++ application/initialize.php
@@ -0,0 +1,7 @@
+<?php
+
+define("APPLICATION_TITLE", "Lily ERP");
+
+// Use these two defines for forcing only SSL (HTTPS) URIs
+// define("DOMAIN", "<domain_name>");
+// define("FORCE_SSL", true);
---
+++ application/views/viewHome.php
@@ -0,0 +1,4 @@
+<div style="font-family:sans-serif;line-height:1.5em">
+<h3><?php echo APPLICATION_TITLE?></h3>
+<p>Simple ERP system</p>
+</div>
