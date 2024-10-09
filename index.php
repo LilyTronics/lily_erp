20fc2b8d7dab74ace5a536db4139349277d11519
Danny van der Pol <danny.vanderpol@teledyne.com>
2024-10-09 13:40:01 +0200

    add calling the framework index php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+// Set variable for framework sub module folder
+define("SUB_MODULE_PATH", "php_framework/");
+
+// Call the framework to process the URI
+include(SUB_MODULE_PATH . "index.php");
