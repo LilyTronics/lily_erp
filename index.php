db35bb01f9ebf265e34736750ff3bb04c8ea39b8
Danny van der Pol <danny.vanderpol@teledyne.com>
2024-10-09 14:06:34 +0200

    renamed submodule define

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
+// define("SHOW_DEBUG", true);
+
 // Set variable for framework sub module folder
-define("SUB_MODULE_PATH", "php_framework/");
+define("SUBMODULE_PATH", "php_framework/");
 
 // Call the framework to process the URI
-include(SUB_MODULE_PATH . "index.php");
+include(SUBMODULE_PATH . "index.php");
