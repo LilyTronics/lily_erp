0bb121c77d3763f1d089cde94dc1362675c6685a
Danny van der Pol <danny.vanderpol@teledyne.com>
2024-10-10 16:34:38 +0200

    add template values for styles

--- application/initialize.php
+++ application/initialize.php
@@ -9,3 +9,9 @@ define("APPLICATION_TITLE", "Lily ERP");
 
 define("CONFIG_FILE", DOC_ROOT . "../lily_erp.ini");
 define("DEBUG_LOG", new ModelSystemLogger("debug"));
+
+define("TEMPLATE_VALUES", [
+    "{BUTTON}" => "w3-button w3-round w3-theme",
+    "{INPUT}"  => "w3-input w3-border w3-round w3-padding-small",
+    "{TABLE}"  => "w3-table width-auto"
+]);
