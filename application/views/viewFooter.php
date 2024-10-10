4a17da22e54ad37303d0e6ce2906ace580e0869e
Danny <danny@lilytronics.nl>
2024-10-10 17:01:29 +0200

    add border and space for footer

--- application/views/viewFooter.php
+++ application/views/viewFooter.php
@@ -1,3 +1,3 @@
-<footer class="w3-container w3-small w3-center">
+<footer class="w3-container w3-margin-top w3-padding w3-border-top w3-small w3-center">
 <?php echo ModelHelper::insertCopyRight("2024"); ?> LilyTronics
 </footer>
