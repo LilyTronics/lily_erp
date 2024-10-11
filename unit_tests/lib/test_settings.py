f375fe255c49e775c25b716cca50e67c5a0bcd44
Danny <danny@lilytronics.nl>
2024-10-11 12:59:57 +0200

    add sql settings for testing

--- unit_tests/lib/test_settings.py
+++ unit_tests/lib/test_settings.py
@@ -9,6 +9,11 @@ class TestSettings:
 
     root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
     uri = "http://localhost:8080/lily_erp/"
+    sql_host = "localhost"
+    sql_database = "lily_erp_test"
+    # These credentials are only used for testing, make sure these are not used in live production servers
+    sql_user = "lily_test"
+    sql_password = "OnlyForTesting!"
 
 
 if __name__ == "__main__":
