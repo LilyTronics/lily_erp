7055b3616c869cdf69ca90f4f50b5ffc9fd191f2
Danny <danny@lilytronics.nl>
2024-10-12 22:17:11 +0200

    improved setup process like log in process

--- application/models/modelSetup.php
+++ application/models/modelSetup.php
@@ -33,7 +33,7 @@ class ModelSetup
         return null;
     }
 
-    public static function createConfiguration($data)
+    public static function createConfiguration($data, $result)
     {
         $fields = [
             [ "host_name",             "host name"                     ],
@@ -47,56 +47,49 @@ class ModelSetup
             [ "admin_repeat_password", "repeat administrator password" ]
         ];
 
-        $result = ["result" => false, "message" => "Failed to create the configuration."];
-        if (isset($data["record"]))
+        $result["message"] = "Failed to create the configuration.";
+        foreach ($fields as $field)
         {
-            $data = $data["record"];
-            foreach ($fields as $field)
+            if (!isset($data[$field[0]]) or $data[$field[0]] == "")
             {
-                if (!isset($data[$field[0]]) or $data[$field[0]] == "")
-                {
-                    $result["message"] = "The {$field[1]} is empty";
-                    return $result;
-                }
-            }
-            if ($data["db_password"] != $data["db_repeat_password"])
-            {
-                $result["message"] = "The repeat database password is not matching the database password";
+                $result["message"] = "The {$field[1]} is empty";
                 return $result;
-            }
-            if ($data["admin_password"] != $data["admin_repeat_password"])
-            {
-                $result["message"] = "The repeat administrator password is not matching the administrator password";
-                return $result;
-            }
-            $result["message"] = "";
-
-            // Data is valid, create configuration file
-            $content = "[sql]\n";
-            $content .= "host={$data["host_name"]}\n";
-            $content .= "database={$data["database"]}\n";
-            $content .= "user={$data["db_user_name"]}\n";
-            $content .= "password=\"{$data["db_password"]}\"\n\n";
-            $fp = fopen(CONFIG_FILE, "w");
-            fwrite($fp, $content);
-            fclose($fp);
-
-            // Add administrator user
-            $record = [
-                "email"    => $data["admin_email"],
-                "name"     => $data["admin_name"],
-                "password" => hash("sha256", $data["admin_password"])
-            ];
-            $user = new ModelDatabaseTableUser();
-            $result["result"] = $user->insertRecord($record);
-            if (!$result["result"])
-            {
-                $result["message"] = "Could not add the administrator to the database";
             }
         }
-        else
+        if ($data["db_password"] != $data["db_repeat_password"])
         {
-            $result["message"] = "No record data received";
+            $result["message"] = "The repeat database password is not matching the database password";
+            return $result;
+        }
+        if ($data["admin_password"] != $data["admin_repeat_password"])
+        {
+            $result["message"] = "The repeat administrator password is not matching the administrator password";
+            return $result;
+        }
+        $result["message"] = "";
+
+        // Data is valid, create configuration file
+        $content = "[sql]\n";
+        $content .= "host={$data["host_name"]}\n";
+        $content .= "database={$data["database"]}\n";
+        $content .= "user={$data["db_user_name"]}\n";
+        $content .= "password=\"{$data["db_password"]}\"\n\n";
+        $fp = fopen(CONFIG_FILE, "w");
+        fwrite($fp, $content);
+        fclose($fp);
+
+        // Add administrator user
+        $user = new ModelDatabaseTableUser();
+        $record = [
+            "email"        => $data["admin_email"],
+            "name"         => $data["admin_name"],
+            "password"     => $user->hash($data["admin_password"]),
+            "access_level" => 7
+        ];
+        $result["result"] = $user->insertRecord($record);
+        if (!$result["result"])
+        {
+            $result["message"] = "Could not add the administrator to the database";
         }
         return $result;
     }
