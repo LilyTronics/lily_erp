2ce9b0f9896a1643f1d67d192d1915d2f014bdf7
Danny <danny@lilytronics.nl>
2024-10-12 18:04:40 +0200

    add function for getting records

--- application/models/modelDatabaseTableBase.php
+++ application/models/modelDatabaseTableBase.php
@@ -13,6 +13,20 @@ class ModelDatabaseTableBase extends ModelDatabaseTable {
                             $autoCreateTable=true, $defaultRecords=[]);
     }
 
+    public function getRecords($filterExpression="", $orderExpression="", $groupExpression="", $start=0, $count=0,
+                               $fields=["*"], $join="", $joinTable="", $joinExpression="", $joinFields=["*"])
+    {
+        $records = $this->selectRecords($filterExpression, $orderExpression, $groupExpression, $start, $count,
+                                        $fields, $join, $joinTable, $joinExpression, $joinFields);
+        if (!$records[0])
+        {
+            DEBUG_LOG->writeMessage("Error getting records for table: {$this->tableName}");
+            DEBUG_LOG->writeMessage($this->getError());
+            return [];
+        }
+        return $records[1];
+    }
+
 }
 
 ?>
