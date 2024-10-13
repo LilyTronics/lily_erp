be8c8327fb98789e9bc68a52f0837c00c5b3d7d1
Danny <danny@lilytronics.nl>
2024-10-13 20:09:04 +0200

    add dashboard page

--- application/controllers/controllerShowPage.php
+++ application/controllers/controllerShowPage.php
@@ -10,12 +10,51 @@ class ControllerShowPage extends ControllerApplication
 
     protected function showDashboard($parameters)
     {
-        return $this->showPage("Dashboard");
+        $pageData = [
+            [
+                "title"   => "<i class=\"fa-solid fa-cash-register w3-margin-right\"></i>Accounting",
+                "uri"     => WEB_ROOT . "accounting",
+                "content" => "some accounting stuff"
+            ],
+            [
+                "title"   => "<i class=\"fa-solid fa-address-card w3-margin-right\"></i>Relations",
+                "uri"     => WEB_ROOT . "relations",
+                "content" => "some relations stuff"
+            ],
+            [
+                "title"   => "<i class=\"fa-solid fa-shop w3-margin-right\"></i>Sales",
+                "uri"     => WEB_ROOT . "sales",
+                "content" => "some sales stuff"
+            ],
+            [
+                "title"   => "<i class=\"fa-solid fa-basket-shopping w3-margin-right\"></i>Purchase",
+                "uri"     => WEB_ROOT . "purchase",
+                "content" => "some purchase stuff"
+            ],
+            [
+                "title"   => "<i class=\"fa-solid fa-warehouse w3-margin-right\"></i>Inventory",
+                "uri"     => WEB_ROOT . "inventory",
+                "content" => "some inventory stuff"
+            ],
+            [
+                "title"   => "<i class=\"fa-solid fa-boxes-stacked w3-margin-right\"></i>Products",
+                "uri"     => WEB_ROOT . "products",
+                "content" => "some products stuff"
+            ],
+            [
+                "title"   => "<i class=\"fa-solid fa-users w3-margin-right\"></i>Users",
+                "uri"     => WEB_ROOT . "users",
+                "content" => "some users stuff"
+            ]
+
+        ];
+        return $this->showPage("Dashboard", $pageData);
     }
 
-    private function showPage($pageName)
+    private function showPage($pageName, $pageData=null)
     {
         $view = $this->createView("view$pageName");
+        $view->setUserData("page_data", $pageData);
         return $view->output();
     }
 
---
+++ application/views/viewDashboard.php
@@ -0,0 +1,21 @@
+<div class="w3-row-padding w3-margin-top w3-margin-bottom">
+<?php
+
+$pageData = $this->getUserData("page_data", []);
+
+foreach ($pageData as $itemData)
+{
+    echo "<div class=\"w3-col m6 l4 w3-margin-top\">\n";
+    echo "<div class=\"w3-card cursor-pointer\" onclick=\"location.href='{$itemData["uri"]}'\">\n";
+    echo "<div class=\"w3-container w3-theme\">\n";
+    echo "<h5>{$itemData["title"]}</h5>\n";
+    echo "</div> <!-- header -->\n";
+    echo "<div class=\"w3-container w3-padding dashboard-item-content\">\n";
+    echo $itemData["content"];
+    echo "</div> <!-- content -->\n";
+    echo "</div> <!-- card -->\n";
+    echo "</div> <!-- col -->\n";
+}
+
+?>
+</div> <!--row -->
