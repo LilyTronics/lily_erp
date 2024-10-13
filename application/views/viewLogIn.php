72f0a756e33fb31bc1a2eb4e495098700affcd23
Danny <danny@lilytronics.nl>
2024-10-13 13:15:27 +0200

    redirect to dashboard after log in

--- application/views/viewLogIn.php
+++ application/views/viewLogIn.php
@@ -4,5 +4,5 @@
 <p><input name="record_field" id="email" class="{INPUT}" type="text" /></p>
 <p>Password:</p>
 <p><input name="record_field" id="password" class="{INPUT}" type="password" /></p>
-<p class="w3-center"><button class="{BUTTON}" onclick="sendForm('log_in', 'Log in')">Log in</button></p>
+<p class="w3-center"><button class="{BUTTON}" onclick="sendForm('log_in', 'Log in', 'dashboard')">Log in</button></p>
 </div>
