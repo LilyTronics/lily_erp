4110f72de869f13efb124c480b6f84ebb2fc00a3
Danny <danny@lilytronics.nl>
2024-10-13 22:45:42 +0200

    add user menu

--- application/views/viewHeader.php
+++ application/views/viewHeader.php
@@ -1,4 +1,10 @@
 <header class="w3-theme w3-container">
 <h2 class="w3-left">Lily ERP</h2>
-<h5 class="w3-right"><i class="fa-solid fa-user w3-margin"></i></h5>
+<span class="w3-dropdown-hover w3-right">
+<h5><i class="fa-solid fa-user w3-margin"></i></h5>
+<div class="w3-dropdown-content w3-bar-block w3-border w3-card" style="right:0">
+<a href="<?php echo WEB_ROOT; ?>my-account" class="w3-bar-item w3-button">My account</a>
+<a href="<?php echo WEB_ROOT; ?>log-out" class="w3-bar-item w3-button">Log out</a>
+</div>
+</span>
 </header>
