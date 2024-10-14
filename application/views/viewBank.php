fc1cac4a4bf9e9bd3cb6908f7153d5945157c8f5
Danny <danny@lilytronics.nl>
2024-10-14 22:13:25 +0200

    add upload form for uploading bank transactions

---
+++ application/views/viewBank.php
@@ -0,0 +1,5 @@
+<form action="<?php echo WEB_ROOT; ?>accounting/bank/upload" method="post" enctype="multipart/form-data">
+<p>Upload export from bank account (MT-940 format):</p>
+<p><input class="{INPUT} width-bank-upload" type="file" name="bank_upload"></p>
+<p><input class="{BUTTON}" type="submit" value="Upload File"></p>
+</form>
