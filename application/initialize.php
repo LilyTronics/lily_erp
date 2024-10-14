74b2acf7f158a3caf0871ec837e63a56e7b40ec9
Danny <danny@lilytronics.nl>
2024-10-14 19:37:08 +0200

    config file location outside web server document root

--- application/initialize.php
+++ application/initialize.php
@@ -13,12 +13,14 @@ define("APPLICATION_TITLE", "Lily ERP");
 // define("DOMAIN", "<domain_name>");
 // define("FORCE_SSL", true);
 
+// Config file must be outside the document root so it can never be reached by a web browser
+define("CONFIG_FILE", $_SERVER["DOCUMENT_ROOT"] . "/../lily_erp.ini");
 
-define("CONFIG_FILE", DOC_ROOT . "../lily_erp.ini");
-define("DEBUG_LOG", new ModelSystemLogger("debug"));
 
 define("TEMPLATE_VALUES", [
     "{BUTTON}" => "w3-button w3-round w3-theme",
     "{INPUT}"  => "w3-input w3-border w3-round w3-padding-small",
     "{TABLE}"  => "w3-table width-auto"
 ]);
+
+define("DEBUG_LOG", new ModelSystemLogger("debug"));
