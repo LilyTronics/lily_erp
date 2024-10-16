d06c7f6710b087eb3ac1d2b5fe7facc53564fe12
Danny <danny@lilytronics.nl>
2024-10-16 11:37:16 +0200

    add authentication check and get records from a table

--- application/controllers/controllerApi.php
+++ application/controllers/controllerApi.php
@@ -3,12 +3,30 @@
 class ControllerApi extends ControllerApplication
 {
 
-    protected $DEFAULT_RESULT = ["result" => false, "message" => "Server error, try again later."];
-
-
     protected function processApiCall($parameters)
     {
-        $result = $this->DEFAULT_RESULT;
+        $result = ["result" => false, "message" => "Server error, try again later."];
+        $postedData = ModelHelper::getPostedData(true);
+
+        $action = "";
+        if (isset($postedData["action"]))
+        {
+            $action = $postedData["action"];
+        }
+        $result["debug"] = $action;
+
+        if (ModelApplicationSession::checkSession() || $action == "log_in" || $action == "log_out") {
+            $result = $this->getResultFromApiCall($postedData, $result);
+        }
+        else
+        {
+            $result["message"] = "Unauthorized";
+        }
+        return json_encode($result, JSON_PRETTY_PRINT);
+    }
+
+    private function getResultFromApiCall($postedData, $result)
+    {
         try
         {
             $postedData = ModelHelper::getPostedData(true);
@@ -22,20 +40,59 @@ class ControllerApi extends ControllerApplication
                         $result = ModelSetup::createConfiguration($postedData["record"], $result);
                     }
                 }
-                if ($postedData["action"] == "log_in")
+                elseif ($postedData["action"] == "log_in")
                 {
                     if (isset($postedData["record"]))
                     {
                         $result = ModelApplicationSession::createSession($postedData["record"], $result);
                     }
                 }
+                elseif (ModelHelper::startsWith($postedData["action"], "get_"))
+                {
+                    $parts = explode("_", $postedData["action"], 2);
+                    if (count($parts) == 2) {
+                        $result = $this->processDataFromDatabase($parts[0], $parts[1]);
+                    }
+                    else
+                    {
+                        $result["message"] = "Invalid action '{$postedData["action"]}'";
+                    }
+                }
+                else
+                {
+                    $result["message"] = "Unknown action '{$postedData["action"]}'";
+                }
+            }
+            else
+            {
+                $result["message"] = "No action defined";
             }
         }
         catch (Exception $e)
         {
             $result["message"] = "Server error:<br/>" . $e->getMessage();
         }
-        return json_encode($result, JSON_PRETTY_PRINT);
+        return $result;
+    }
+
+    private function processDataFromDatabase($action, $tableName)
+    {
+        $result = ["result" => false, "message" => "Error reading table '$tableName'"];
+        $table = new ModelDatabaseTableBase($tableName);
+
+        switch($action)
+        {
+            case "get":
+                $result["records"] = $table->getRecords();
+                $result["result"] = true;
+                $result["message"] = "";
+                break;
+
+            default:
+                $result["message"] = "Invalid table action '$action'";
+        }
+
+        return $result;
     }
 
 }
