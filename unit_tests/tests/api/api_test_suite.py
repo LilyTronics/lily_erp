ec8d78c78aa2c7617fde8bdc6dcf1fce7221eb33
Danny <danny@lilytronics.nl>
2024-10-16 12:02:43 +0200

    add test code for the API test suite

--- unit_tests/tests/api/api_test_suite.py
+++ unit_tests/tests/api/api_test_suite.py
@@ -18,3 +18,17 @@ class ApiTestSuite(lily_unit_test.TestSuite):
         self.log.debug(f"Result: {result["result"]}")
         self.fail_if(not result["result"], f"Error message: {result["message"]}")
         return result
+
+
+if __name__ == "__main__":
+
+    class _TestApiTestSuite(ApiTestSuite):
+
+        def test_get_user(self):
+            data = {
+                "action": "get_user"
+            }
+            result = self._do_api_call(data)
+            self.log.debug(f"Records: {len(result["records"])}")
+
+    _TestApiTestSuite().run()
