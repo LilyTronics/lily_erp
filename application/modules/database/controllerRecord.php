81911afdaacdb0ef55127c093cc9de21a1c20d56
Danny <danny@lilytronics.nl>
2024-10-18 21:59:14 +0200

    fix show record

--- application/modules/database/controllerRecord.php
+++ application/modules/database/controllerRecord.php
@@ -5,13 +5,8 @@ class ControllerRecord extends ControllerApplication
 
     protected function showRecord($parameters)
     {
-        return $this->showPage("Record");
-    }
-
-    private function showPage($pageName)
-    {
-        $view = $this->createView("view$pageName");
-        return $view->output();
+        $pageData = [];
+        return $this->showPage("database/viewRecord", $pageData);
     }
 
 }
