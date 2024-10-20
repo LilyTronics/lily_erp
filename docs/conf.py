4b292180615eaad7dd398bcca9d7306f9660d5bf
Danny <danny@lilytronics.nl>
2024-10-20 20:29:45 +0200

    add builder for building documentation

---
+++ docs/conf.py
@@ -0,0 +1,26 @@
+"""
+Configuration file for the Sphinx documentation builder.
+
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+
+# Disable message for naming convention in this file,
+# because we must comply to the sphinx naming convention
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+project = 'lily-erp'
+project_copyright = '2024, LilyTronics'
+author = 'LilyTronics'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
---
+++ docs/index.rst
@@ -0,0 +1,14 @@
+Lily ERP (Community Edition)
+==============================================
+
+The is the documentation for the Lily ERP system.
+
+This documentation will guide your throught the setup and usage of the ERP system.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+    Home                            <self>
+    Installation                    <installation.rst>
