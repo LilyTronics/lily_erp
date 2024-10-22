bbd7e931d60dc3a7899f976913ff92517e0e5982
Danny <danny@lilytronics.nl>
2024-10-22 07:46:34 +0200

    add link to the documentation

--- application/views/viewFooter.php
+++ application/views/viewFooter.php
@@ -1,3 +1,4 @@
 <footer class="w3-container w3-margin-top w3-padding w3-border-top w3-small w3-center">
-<?php echo ModelHelper::insertCopyRight("2024"); ?> LilyTronics
+<?php echo ModelHelper::insertCopyRight("2024"); ?> LilyTronics<br />
+<a href="https://lily-erp.readthedocs.io/" target="_blank">documentation</a>
 </footer>
