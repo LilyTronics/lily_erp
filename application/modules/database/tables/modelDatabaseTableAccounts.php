299cfb4425e825ece3368574080d741d45153104
Danny <danny@lilytronics.nl>
2024-10-23 22:27:41 +0200

    add fields for the accounts

--- application/modules/database/tables/modelDatabaseTableAccounts.php
+++ application/modules/database/tables/modelDatabaseTableAccounts.php
@@ -8,7 +8,19 @@ class ModelDatabaseTableAccounts extends ModelDatabaseTableBase {
             [ "Name"    => "id",
               "Type"    => "INT",
               "Options" => "AUTO_INCREMENT",
-              "Key"     => true ]
+              "Key"     => true ],
+            [ "Name"    => "parent_id",
+              "Type"    => "INT"],
+            [ "Name"    => "level",
+              "Type"    => "INT"],
+            [ "Name"    => "number",
+              "Type"    => "VARCHAR(200)"],
+            [ "Name"    => "name",
+              "Type"    => "VARCHAR(200)"],
+            [ "Name"    => "debit_credit",
+              "Type"    => "VARCHAR(1)"],
+            [ "Name"    => "category",
+              "Type"    => "VARCHAR(200)"]
         ];
         parent::__construct($tableName, $fields, true);
     }
