4c774d89c6646dec9f2c478c148224a79b2f2fbb
Danny <danny@lilytronics.nl>
2024-10-23 10:04:28 +0200

    add script for creating demo data to the database

---
+++ unit_tests/demo_data/load_demo_data.py
@@ -0,0 +1,33 @@
+"""
+This script loads the demo data into the test database.
+
+!!! This will delete all the data from the test database !!!
+"""
+
+from unit_tests.lib.api import Api
+from unit_tests.lib.database import Database
+from unit_tests.lib.test_settings import TestSettings
+
+
+Database.clear_all()
+
+# Create from scratch
+data = {
+    "action": "create_configuration",
+    "record": {
+        "host_name": TestSettings.sql_host,
+        "database": TestSettings.sql_database,
+        "db_user_name": TestSettings.sql_user,
+        "db_password": TestSettings.sql_password,
+        "db_repeat_password": TestSettings.sql_password,
+        "admin_email": TestSettings.admin_email,
+        "admin_name": TestSettings.admin_name,
+        "admin_password": TestSettings.admin_password,
+        "admin_repeat_password": TestSettings.admin_password
+    }
+}
+
+response = Api.do_api_call(data, False)
+if not response["result"]:
+    print(f"Cannot create configuration: {response["message"]}")
+    exit()
