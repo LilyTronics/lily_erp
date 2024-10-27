43668d6c7234eca820cf026607d2cfe38105a8a3
Danny <danny@lilytronics.nl>
2024-10-27 21:00:24 +0100

    add test suite for account table

--- unit_tests/tests/api/api_test_suite.py
+++ unit_tests/tests/api/api_test_suite.py
@@ -10,6 +10,9 @@ from unit_tests.lib.database import Database
 
 class ApiTestSuite(lily_unit_test.TestSuite):
 
+    # Must be overridden in the sub class
+    table_name = "unknown"
+
     _api = None
 
     def setup(self):
@@ -19,9 +22,35 @@ class ApiTestSuite(lily_unit_test.TestSuite):
 
     def do_api_call(self, data, check_log_in=True, check_result=True):
         result =  self._api.do_api_call(data, check_log_in)
-        self.log.debug(f"Result: {result["result"]}")
         self.fail_if(check_result and not result["result"], f"Error message: {result["message"]}")
         return result
+
+    def get_records(self):
+        data = {
+            "action": f"get_{self.table_name}",
+        }
+        return self.do_api_call(data)
+
+    def add_record(self, record):
+        data = {
+            "action": f"add_{self.table_name}",
+            "record": record
+        }
+        return self.do_api_call(data)
+
+    def update_record(self, record):
+        data = {
+            "action": f"update_{self.table_name}",
+            "record": record
+        }
+        return self.do_api_call(data)
+
+    def delete_record(self, record):
+        data = {
+            "action": f"delete_{self.table_name}",
+            "record": record
+        }
+        return self.do_api_call(data)
 
 
 if __name__ == "__main__":
