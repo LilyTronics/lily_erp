e41035d32906420fe94c04422f259c79beef9881
Danny <danny@lilytronics.nl>
2024-10-30 16:09:13 +0100

    removed not working table

--- application/modules/accounting/viewBank.php
+++ application/modules/accounting/viewBank.php
@@ -1,9 +1,3 @@
-<?php
-
-$pageData = $this->getUserData("page_data", []);
-
-?>
-<div class="w3-container w3-section w3-responsive" id="record-table"></div>
 <div class="w3-container w3-section w3-border-top">
 <form action="<?php echo WEB_ROOT; ?>accounting/bank/upload" method="post" enctype="multipart/form-data">
 <p>Upload export from bank account (MT-940 format):</p>
@@ -11,18 +5,3 @@ $pageData = $this->getUserData("page_data", []);
 <p><input class="{BUTTON}" type="submit" value="Upload File"></p>
 </form>
 </div>
-<script>
-
-'use strict';
-
-showTable('accounting', 'bank_transaction');
-
-<?php
-
-if (isset($pageData["upload_message"])) {
-    echo "showModal('Upload', '{$pageData["upload_message"]}');\n";
-}
-
-?>
-
-</script>
