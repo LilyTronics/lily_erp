30ba9061fd165f2530f332224f2a1678be004f33
Danny <danny@lilytronics.nl>
2024-10-30 16:18:52 +0100

    show loader when new account is clicked

--- application/modules/accounting/viewChartOfAccounts.php
+++ application/modules/accounting/viewChartOfAccounts.php
@@ -1,3 +1,3 @@
 <div class="w3-container">
-<a class="{TOOL_BUTTON}" href="<?php echo WEB_ROOT; ?>record/accounting/account/0">New account</a>
+<a class="{TOOL_BUTTON}" href="<?php echo WEB_ROOT; ?>record/accounting/account/0" onclick="showModalLoader()">New account</a>
 </div>
