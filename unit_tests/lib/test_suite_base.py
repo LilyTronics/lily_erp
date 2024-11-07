d61dd105d4464e57ab62402905e4bd660ae81539
Danny <danny@lilytronics.nl>
2024-11-07 13:08:39 +0100

    add clear database to setup

--- unit_tests/lib/test_suite_base.py
+++ unit_tests/lib/test_suite_base.py
@@ -4,6 +4,7 @@ Our own test suite.
 
 from lily_unit_test import TestSuite
 
+from unit_tests.lib.database import Database
 from unit_tests.lib.http_request import HttpRequest
 
 
@@ -11,8 +12,10 @@ class TestSuiteBase(TestSuite):
 
     http_request = None
 
-    def setup(self):
+    def setup(self, drop_user=False):
         self.http_request = HttpRequest()
+        Database.clear_all(drop_user)
+        Database.create_default_user()
 
     def test_http_request(self):
         r = self.http_request.do_get()
