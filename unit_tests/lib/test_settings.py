3fa49b517978c65e4ea208d12f6239a885de0928
Danny <danny@lilytronics.nl>
2024-11-07 09:25:10 +0100

    updated settings

--- unit_tests/lib/test_settings.py
+++ unit_tests/lib/test_settings.py
@@ -10,12 +10,13 @@ class TestSettings:
     root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
     ini_filename = os.path.abspath(os.path.join(root_path, "../../lily_erp.ini"))
     uri = "http://localhost:8080/lily_erp/"
-    sql_host = "localhost"
-    sql_database = "lily_erp_test"
 
     # These credentials are only used for testing, make sure these are not used in live production servers
+    sql_host =  "localhost"
+    sql_database = "lily_erp_test"
     sql_user = "lily_test"
     sql_password = "OnlyForTest!"
+
     admin_email = "admin@lily-erp.nl"
     admin_name = "Lily Erp Admin"
     admin_password = "LilyErp"
