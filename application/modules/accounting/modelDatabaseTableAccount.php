837474c7f25dc1a7df8ae7d5f0f29ad7c5096354
Danny <danny@lilytronics.nl>
2024-11-18 12:46:32 +0100

    add required property for the fields

--- application/modules/accounting/modelDatabaseTableAccount.php
+++ application/modules/accounting/modelDatabaseTableAccount.php
@@ -5,12 +5,12 @@ class ModelDatabaseTableAccount extends ModelDatabaseTableBase {
     public function __construct() {
         $this->tableName = "account";
 
-        $this->fields[] = [ "Name" => "parent_id",    "Type" => "INT"          ];
-        $this->fields[] = [ "Name" => "level",        "Type" => "INT"          ];
-        $this->fields[] = [ "Name" => "number",       "Type" => "VARCHAR(200)" ];
-        $this->fields[] = [ "Name" => "name",         "Type" => "VARCHAR(200)" ];
-        $this->fields[] = [ "Name" => "debit_credit", "Type" => "VARCHAR(1)"   ];
-        $this->fields[] = [ "Name" => "category",     "Type" => "VARCHAR(200)" ];
+        $this->fields[] = [ "Name" => "parent_id",    "Type" => "INT"         , "Required" => true  ];
+        $this->fields[] = [ "Name" => "level",        "Type" => "INT"         , "Required" => true  ];
+        $this->fields[] = [ "Name" => "number",       "Type" => "VARCHAR(200)", "Required" => true  ];
+        $this->fields[] = [ "Name" => "name",         "Type" => "VARCHAR(200)", "Required" => true  ];
+        $this->fields[] = [ "Name" => "debit_credit", "Type" => "VARCHAR(1)"  , "Required" => true  ];
+        $this->fields[] = [ "Name" => "category",     "Type" => "VARCHAR(200)", "Required" => false ];
 
         // $this->inputs["field_name"] = ["type" => "text"];
 
--- application/modules/database/modelDatabaseTableTemplate.php
+++ application/modules/database/modelDatabaseTableTemplate.php
@@ -5,7 +5,7 @@ class ModelDatabaseTableTemplate extends ModelDatabaseTableBase {
     public function __construct() {
         $this->tableName = "template";
 
-        // $this->fields[] = [ "Name" => "field_name", "Type" => "field_type" ];
+        // $this->fields[] = [ "Name" => "field_name", "Type" => "field_type", "Required" => true ];
 
         // $this->inputs["field_name"] = ["type" => "text"];
 
