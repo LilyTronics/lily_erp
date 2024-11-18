db87f66a8764d4350c314d361cee584e8d308cbf
Danny <danny@lilytronics.nl>
2024-11-18 21:50:48 +0100

    force SSL

--- application/config.php
+++ application/config.php
@@ -3,7 +3,7 @@
 date_default_timezone_set("Europe/Amsterdam");
 
 // Define your domain name here to make SSL work properly
-define("DOMAIN", "lilytronics");
+define("DOMAIN", "erp.lilytronics.nl");
 
 // Config file must be outside the document root so it can never be reached by a web browser
 define("CONFIG_FILE", $_SERVER["DOCUMENT_ROOT"] . "/../lily_erp.ini");
