3bd45c403a8aeed4fad3d79bcd618d3d8b7211e9
Danny <danny@lilytronics.nl>
2024-11-21 20:51:31 +0100

    comment out the upload form, needs fixing

--- application/modules/accounting/viewBank.php
+++ application/modules/accounting/viewBank.php
@@ -1,7 +1,5 @@
-<div class="">
-<form action="<?php echo WEB_ROOT; ?>accounting/bank/upload" method="post" enctype="multipart/form-data">
+<!-- <form action="<?php echo WEB_ROOT; ?>accounting/bank/upload" method="post" enctype="multipart/form-data">
 <p>Upload export from bank account (MT-940 format):</p>
 <p><input class="{INPUT} width-bank-upload" type="file" name="bank_upload"></p>
 <p><input class="{BUTTON}" type="submit" value="Upload File"></p>
-</form>
-</div>
+</form> -->
