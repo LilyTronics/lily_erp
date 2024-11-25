a813132fabdec5daca0c38df572e3f404843c0ed
Danny <danny@lilytronics.nl>
2024-11-25 11:47:26 +0100

    updated template values

--- application/views/viewCreateConfig.php
+++ application/views/viewCreateConfig.php
@@ -16,11 +16,11 @@ $adminName = isset($pageData["record"]["admin_name"]) ? $pageData["record"]["adm
 <p>You can also use an existing database, but it must be empty. All tables must be removed ('dropped').</p>
 <p>Also create a MySQL user for accessing the database with all privileges enabled.</p>
 <p>When ready, fill out the following form and the configuration will be created for you.</p>
-<form action="<?php echo WEB_ROOT; ?>api" method="post">
+<form action="{API_URI}" method="post">
 <input type="hidden" name="on_success" value="" />
 <input type="hidden" name="on_failure" value="setup/create-config" />
 <input type="hidden" name="title" value="Create configuration" />
-<table class="{TABLE}">
+<table>
 <tr><td>Host name:</td><td><input name="record[host_name]" class="{INPUT}" type="text" value="<?php echo $hostName; ?>" /></td></tr>
 <tr><td>Database name:</td><td><input name="record[database]" class="{INPUT}" type="text" value="<?php echo $database; ?>" /></td></tr>
 <tr><td>Database user name:</td><td><input name="record[db_user_name]" class="{INPUT}" type="text" value="<?php echo $dbUserName; ?>" /></td></tr>
@@ -29,7 +29,7 @@ $adminName = isset($pageData["record"]["admin_name"]) ? $pageData["record"]["adm
 </table>
 <p>An administrator user needs to be setup for log in and creating user accounts.</p>
 <p>Enter the information for the administrator user in the form below.</p>
-<table class="{TABLE}">
+<table>
 <tr><td>Email address:</td><td><input name="record[admin_email]" class="{INPUT}" type="text" value="<?php echo $adminEmail; ?>" /></td></tr>
 <tr><td>Name:</td><td><input name="record[admin_name]" class="{INPUT}" type="text" value="<?php echo $adminName; ?>" /></td></tr>
 <tr><td>Password:</td><td><input name="record[admin_password]" class="{INPUT}" type="password" autocomplete="new-password" /></td></tr>
