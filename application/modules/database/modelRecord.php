8f599a3d05b333f65d230b4fece716fa8649356e
Danny <danny@lilytronics.nl>
2024-12-05 22:17:36 +0100

    add debit and credit field for number formatting

--- application/modules/database/modelRecord.php
+++ application/modules/database/modelRecord.php
@@ -5,18 +5,25 @@ class ModelRecord
 
     public static function formatFieldName($field, $ucFirst=false)
     {
-        $name = str_replace("_", " ", $field);
-        if ($ucFirst)
+        if ($field == "debit_credit")
         {
-            $name = ucfirst($name);
+            $name = "D/C";
+        }
+        else
+        {
+            $name = str_replace("_", " ", $field);
+            if ($ucFirst)
+            {
+                $name = ucfirst($name);
+            }
         }
         return $name;
     }
 
     public static function formatValue($field, $value)
     {
-        $value = ($value == null ? "" : $value);
-        if ($field == "amount")
+        $value = ($value === null ? "" : $value);
+        if ($field == "amount" or $field == "debit" or $field == "credit")
         {
             $value = preg_replace("/(\.\d{2}[^0]*)(0*)$/", "$1", $value);
         }
