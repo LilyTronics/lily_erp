527cdb8042b928f52ee295cad2350acf33c489e3
Danny <danny@lilytronics.nl>
2024-12-06 12:42:58 +0100

    show user statistics on the dashboard

--- application/modules/users/modelUsers.php
+++ application/modules/users/modelUsers.php
@@ -22,7 +22,35 @@ class ModelUsers
     }
 
     private function getDashboardContent() {
-        return [];
+        $table = new ModelDatabaseTableUser();
+        $nActivated = count($table->getRecords("is_active = 1"));
+        $nDeactivated = count($table->getRecords("is_active = 0"));
+        $pastMonth = time() - (60 * 60 * 24 * 30);
+        $nActive = count($table->getRecords("last_log_in >= {$pastMonth}"));
+        $nNotActive = count($table->getRecords("last_log_in < {$pastMonth}"));
+        $messages = [
+            [
+                "icon"    => "{ICON_INFORMATION}",
+                "message" => "{$nActivated} activated users",
+                "link"    => "users"
+            ],
+            [
+                "icon"    => "{ICON_INFORMATION}",
+                "message" => "{$nDeactivated} deactivated users",
+                "link"    => "users"
+            ],
+            [
+                "icon"    => "{ICON_INFORMATION}",
+                "message" => "{$nActive} active users in the last 30 days",
+                "link"    => "users"
+            ],
+            [
+                "icon"    => "{ICON_INFORMATION}",
+                "message" => "{$nNotActive} not active users in the last 30 days",
+                "link"    => "users"
+            ]
+        ];
+        return $messages;
     }
 
 }
