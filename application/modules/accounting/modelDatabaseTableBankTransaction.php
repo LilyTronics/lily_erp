cbb342c09ab69b446cebf064763d8320693ea2e0
Danny <danny@lilytronics.nl>
2024-12-06 20:57:27 +0100

    use decimal constant

--- application/modules/accounting/modelDatabaseTableBankTransaction.php
+++ application/modules/accounting/modelDatabaseTableBankTransaction.php
@@ -5,16 +5,16 @@ class ModelDatabaseTableBankTransaction extends ModelDatabaseTableBase {
     public function __construct() {
         $this->tableName = "bank_transaction";
 
-        $this->fields[] = [ "name" => "reference",        "type" => "VARCHAR(200)"  ];
-        $this->fields[] = [ "name" => "own_account",      "type" => "VARCHAR(200)"  ];
-        $this->fields[] = [ "name" => "date",             "type" => "DATE"          ];
-        $this->fields[] = [ "name" => "debit_credit",     "type" => "VARCHAR(1)"    ];
-        $this->fields[] = [ "name" => "amount",           "type" => "DECIMAL(18,5)" ];
-        $this->fields[] = [ "name" => "transaction_type", "type" => "VARCHAR(200)"  ];
-        $this->fields[] = [ "name" => "counter_account",  "type" => "VARCHAR(200)"  ];
-        $this->fields[] = [ "name" => "counter_name",     "type" => "VARCHAR(200)"  ];
-        $this->fields[] = [ "name" => "description",      "type" => "VARCHAR(200)"  ];
-        $this->fields[] = [ "name" => "state",            "type" => "VARCHAR(200)"  ];
+        $this->fields[] = [ "name" => "reference",        "type" => "VARCHAR(200)"     ];
+        $this->fields[] = [ "name" => "own_account",      "type" => "VARCHAR(200)"     ];
+        $this->fields[] = [ "name" => "date",             "type" => "DATE"             ];
+        $this->fields[] = [ "name" => "debit_credit",     "type" => "VARCHAR(1)"       ];
+        $this->fields[] = [ "name" => "amount",           "type" => self::TYPE_DECIMAL ];
+        $this->fields[] = [ "name" => "transaction_type", "type" => "VARCHAR(200)"     ];
+        $this->fields[] = [ "name" => "counter_account",  "type" => "VARCHAR(200)"     ];
+        $this->fields[] = [ "name" => "counter_name",     "type" => "VARCHAR(200)"     ];
+        $this->fields[] = [ "name" => "description",      "type" => "VARCHAR(200)"     ];
+        $this->fields[] = [ "name" => "state",            "type" => "VARCHAR(200)"     ];
 
         parent::__construct(true);
     }
