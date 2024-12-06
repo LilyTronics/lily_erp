4a91932ffb5d238508e95b0a9178b6d11e44d6ff
Danny <danny@lilytronics.nl>
2024-12-06 12:02:25 +0100

    restyled the dashboard page a bit

--- application/views/viewDashboard.php
+++ application/views/viewDashboard.php
@@ -8,13 +8,13 @@ if (isset($pageData["items"]))
     foreach ($pageData["items"] as $itemData)
     {
         $link = WEB_ROOT . $itemData["link"];
-        echo "<div class=\"card shadow-sm float-start dashboard-item m-3\">\n";
+        echo "<div class=\"card shadow-sm float-start dashboard-item m-2\">\n";
         echo "<div class=\"theme-bg p-1 rounded-top fs-5\">\n";
         echo "<a class=\"no-link-color d-block\" href=\"{$link}\" {LNK_SHOW_LOADER}>";
         echo "<i class=\"{$itemData["icon"]} mx-2\"></i>{$itemData["title"]}\n";
         echo "</a>\n";
         echo "</div> <!-- header -->\n";
-        echo "<div class=\"p-2 small\">\n";
+        echo "<div class=\"p-2\">\n";
         // Content should be an array with items (icon, message)
         if (count($itemData["content"]) > 0)
         {
