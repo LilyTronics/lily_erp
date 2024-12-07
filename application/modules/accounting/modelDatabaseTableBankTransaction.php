91e74cf4139eb019e257a38e3079e43bdef9f3da
Danny <danny@lilytronics.nl>
2024-12-07 21:35:23 +0100

    set inputs for bank transactions

--- application/modules/accounting/modelDatabaseTableBankTransaction.php
+++ application/modules/accounting/modelDatabaseTableBankTransaction.php
@@ -5,16 +5,21 @@ class ModelDatabaseTableBankTransaction extends ModelDatabaseTableBase {
     public function __construct() {
         $this->tableName = "bank_transaction";
 
-        $this->fields[] = [ "name" => "reference",        "type" => "VARCHAR(200)"     ];
-        $this->fields[] = [ "name" => "own_account",      "type" => "VARCHAR(200)"     ];
-        $this->fields[] = [ "name" => "date",             "type" => "DATE"             ];
-        $this->fields[] = [ "name" => "debit_credit",     "type" => "VARCHAR(1)"       ];
-        $this->fields[] = [ "name" => "amount",           "type" => self::TYPE_DECIMAL ];
-        $this->fields[] = [ "name" => "transaction_type", "type" => "VARCHAR(200)"     ];
-        $this->fields[] = [ "name" => "counter_account",  "type" => "VARCHAR(200)"     ];
-        $this->fields[] = [ "name" => "counter_name",     "type" => "VARCHAR(200)"     ];
-        $this->fields[] = [ "name" => "description",      "type" => "VARCHAR(200)"     ];
-        $this->fields[] = [ "name" => "state",            "type" => "VARCHAR(200)"     ];
+        $this->fields[] = ["name" => "reference",        "type" => "VARCHAR(200)",     "required" => true];
+        $this->fields[] = ["name" => "own_account",      "type" => "VARCHAR(200)",     "required" => true];
+        $this->fields[] = ["name" => "date",             "type" => "DATE",             "required" => true];
+        $this->fields[] = ["name" => "debit_credit",     "type" => "VARCHAR(1)",       "required" => true];
+        $this->fields[] = ["name" => "amount",           "type" => self::TYPE_DECIMAL, "required" => true];
+        $this->fields[] = ["name" => "transaction_type", "type" => "VARCHAR(200)",     "required" => true];
+        $this->fields[] = ["name" => "counter_account",  "type" => "VARCHAR(200)",     "required" => true];
+        $this->fields[] = ["name" => "counter_name",     "type" => "VARCHAR(200)",     "required" => true];
+        $this->fields[] = ["name" => "description",      "type" => "VARCHAR(200)",     "required" => true];
+        $this->fields[] = ["name" => "state",            "type" => "VARCHAR(200)",     "required" => true];
+
+        foreach ($this->fields as $field)
+        {
+            $this->inputs[$field["name"]] = [];
+        }
 
         parent::__construct(true);
     }
