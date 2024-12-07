9e869d82e22f6c732587559de3902fbf69e4e529
Danny <danny@lilytronics.nl>
2024-12-07 20:33:49 +0100

    add icons for in table add record

--- application/templateValues.php
+++ application/templateValues.php
@@ -7,15 +7,18 @@ $GLOBALS["TEMPLATE_VALUES"] = [
     "{CONTAINER}"           => "container-fluid p-3",
     "{BUTTON}"              => "btn px-3 py-1 theme-btn",
     "{BUTTON_RED}"          => "btn px-3 py-1 btn-danger",
+    "{BUTTON_SMALL}"        => "btn px-2 py-1 theme-btn",
     "{INPUT}"               => "form-control",
     "{BTN_SHOW_LOADER}"     => "data-bs-toggle=\"modal\" data-bs-target=\"#modal-loader\"",
     "{LNK_SHOW_LOADER}"     => "onclick=\"showLoader()\"",
 
     // Icons
+    "{ICON_CHECK}"          => "<i class=\"fa-solid fa-check\"></i>",
     "{ICON_CHECK_OK}"       => "<i class=\"fa-solid fa-circle-check text-success\"></i>",
     "{ICON_EXCLAMATION}"    => "<i class=\"fa-solid fa-circle-exclamation text-warning\"></i>",
     "{ICON_INFORMATION}"    => "<i class=\"fa-solid fa-circle-info text-info\"></i>",
     "{ICON_USER}"           => "<i class=\"fa-solid fa-user\"></i>",
+    "{ICON_XMARK}"          => "<i class=\"fa-solid fa-xmark\"></i>",
 
     // Various
     "{WEB_ROOT}"            => WEB_ROOT,
