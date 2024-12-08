9134a1239be58080174f9ec1434ade7f7f4eae34
Danny <danny@lilytronics.nl>
2024-12-08 12:21:16 +0100

    updated documentation configuration

--- docs/conf.py
+++ docs/conf.py
@@ -22,5 +22,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-# html_static_path = ['_static']
 html_theme = "sphinx_rtd_theme"
+html_title ="Lily ERP"
+html_copy_source = False
+html_show_sphinx = False
