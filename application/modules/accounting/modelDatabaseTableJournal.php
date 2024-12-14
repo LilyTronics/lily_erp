58c941a1a203d393c36b3faca7431ba641f45243
Danny <danny@lilytronics.nl>
2024-12-14 12:18:55 +0100

    add field for linked item

--- application/modules/accounting/modelDatabaseTableJournal.php
+++ application/modules/accounting/modelDatabaseTableJournal.php
@@ -10,12 +10,14 @@ class ModelDatabaseTableJournal extends ModelDatabaseTableBase {
         $this->fields[] = ["name" => "description", "type" => "VARCHAR(200)",     "required" => true];
         $this->fields[] = ["name" => "debit",       "type" => self::TYPE_DECIMAL, "required" => false];
         $this->fields[] = ["name" => "credit",      "type" => self::TYPE_DECIMAL, "required" => false];
+        $this->fields[] = ["name" => "linked_item", "type" => "VARCHAR(200)",     "required" => false];
 
         $this->inputs["date"]        = ["type" => "date"];
         $this->inputs["account_id"]  = ["type" => "list", "data" => "account"];
         $this->inputs["description"] = ["type" => "text", "width" => "large"];
         $this->inputs["debit"]       = ["type" => "text", "width" => "small"];
         $this->inputs["credit"]      = ["type" => "text", "width" => "small"];
+        $this->inputs["linked_item"] = ["type" => "text"];
 
         $this->defaultOrder = "date DESC";
 
